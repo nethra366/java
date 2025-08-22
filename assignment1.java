5351a214d23184305a77e77b5c50b1f6fe927429
nethra366 <nethra3621@gmail.com>
2025-08-22 13:22:50 +0530

    Update and rename README.md to assignment1.java

---
+++ assignment1.java
@@ -0,0 +1,31 @@
+# java
+package oops;
+import java.util.*;
+public class Main {
+
+		
+		    static class Book {
+		        String title, author;
+		        Book(String t,String a){ title=t; author=a; }
+		        public String toString(){ return title+" by "+author; }
+		    }
+
+		    public static void main(String[] args) {
+		        ArrayList<Book> books=new ArrayList<>();
+		        books.add(new Book("Java","James"));
+		        books.add(new Book("DSA","Mark"));
+
+		        try {
+		            System.out.println("Found: "+search(books,"Java"));
+		            System.out.println("Found: "+search(books,"Python"));
+		        } catch(Exception e){ 
+		            System.out.println(e.getMessage()); 
+		        }
+		    }
+
+		    static Book search(ArrayList<Book> books,String t) throws Exception {
+		        for(Book b:books) if(b.title.equalsIgnoreCase(t)) return b;
+		        throw new Exception("Book '"+t+"' not found");
+		    }
+		
+	}
